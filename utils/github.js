af30bcfbc4650b983717911fd99ac73248a4e49a
IMXNOOBX <69653071+IMXNOOBX@users.noreply.github.com>
2022-06-05 01:40:43 +0200

    1.0.3

--- utils/github.js
+++ utils/github.js
@@ -1,12 +1,12 @@
 module.exports = function(client) {
 	const app = client.express()
-	app.use(client.express.json())
+	app.use(client.bodyParser.json())
 	app.use(client.github);
 	// app.use(client.express.urlencoded({ extended: true }))
 
-	webhookHandler.on('*', function (event, repo, data) {
+	client.github.on('*', function (event, repo, data) {
 		for(var i; i < client.config.github.events.length; i++){
-			if(client.config.github.events[i].name === event){
+			if(client.config.github.events[i] === event){
 				client.log.console(`[Github] | ${event} event has been triggered`);
 			}
 		}
--- utils/twitter.js
+++ utils/twitter.js
@@ -1,8 +1,34 @@
 
 class twitter {
-	static twitterFunction(client, param) {
-		client.log.console(param)
+	static async tweet(client, string, media = null, poll = null) {
+		try {
+			var data;
+			let mid = media ? await client.v1.uploadMedia(media) : null;
+			// let data = await client.v2.tweet(string, { poll: poll, media_ids: mid});
+			if (poll) {
+				data = await client.tclient.v2.tweet('twitter-api-v2 is awesome!', { poll: poll }); //  poll: { duration_minutes: 120, options: ['Absolutely', 'For sure!'] }
+			} else {
+				data = await client.tclient.v2.tweet(string, {media_ids: mid});
+			}
+			return data;
+		} catch (err) {
+			client.log.error(err);
+		}
 	}
+	static async getUser(client, user) {
+		try {
+			var data;
+			if(isNaN(name)){
+				data = await client.tclient.v2.user(user);
+			} else {
+				data = await client.v2.userByUsername(user);
+			}
+			return data;
+		} catch (err) {
+			client.log.error(err);
+		}
+	}
+
 };
 
 module.exports = twitter;
