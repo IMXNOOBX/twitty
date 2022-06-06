fae80bb0f866c0739f250bf480d535a3f620b5cf
IMXNOOBX <69653071+IMXNOOBX@users.noreply.github.com>
2022-06-06 14:37:09 +0200

    1.0.5

--- utils/twitter.js
+++ utils/twitter.js
@@ -5,7 +5,7 @@ class twitter {
 			var me = await client.tclient.v2.me(), me = me.data
 			var linebreak = ''
 			string.split("\\n").forEach(lb => { // ik it's poor, but it works
-				linebreak += '\n'+ `${lb}`;
+				linebreak += '\n' + `${lb}`;
 			})
 			var data = await client.tclient.v2.tweet(linebreak, poll ? { poll: poll } : null);
 			return { data: data?.data, me };
@@ -16,8 +16,14 @@ class twitter {
 	static async tweetaction(client, tweet, action, message) {
 		try {
 			var data
+			var linebreak = ''
+			if (message) {
+				string.split("\\n").forEach(lb => {
+					linebreak += '\n' + `${lb}`;
+				})
+			}
 			var me = await client.tclient.v2.me(), me = me.data
-			if (action == "like") 
+			if (action == "like")
 				data = await client.tclient.v2.like(me.id, tweet);
 			else if (action == "dislike")
 				data = await client.tclient.v2.unlike(me.id, tweet);
@@ -26,7 +32,7 @@ class twitter {
 			else if (action == "unretweet")
 				data = await client.tclient.v2.unretweet(me.id, tweet);
 			else if (action == "reply")
-				data = await client.tclient.v2.reply(message, tweet);
+				data = await client.tclient.v2.reply(linebreak, tweet);
 			else if (action == "delete")
 				data = await client.tclient.v2.deleteTweet(me.id, tweet);
 			return data?.data;
@@ -45,12 +51,12 @@ class twitter {
 	static async getUser(client, user) {
 		try {
 			var data;
-			if(isNaN(user)){ // ima be honset, i didnt know that isNaN(user) stands for "is not a number"
+			if (isNaN(user)) { // ima be honset, i didnt know that isNaN(user) stands for "is not a number"
 				data = await client.tclient.v2.userByUsername(user);
 			} else {
 				data = await client.tclient.v2.user(user);
 			}
-				// var data2 = await client.tclient.v2.userLikedTweets(data.data.id);
+			// var data2 = await client.tclient.v2.userLikedTweets(data.data.id);
 			return data?.data;
 		} catch (err) {
 			client.log.error(err);
